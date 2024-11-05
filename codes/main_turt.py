9a1789219d1b3ff187714d40bac9216afc831d42
chrinilo <christian.louka@icloud.com>
2024-11-05 11:14:02 +0100

    a

--- codes/main_turt.py
+++ codes/main_turt.py
@@ -11,7 +11,7 @@ class entity:
         def __init__(self):
             self.t = turtle.Turtle("assets/player.gif")
         
-        def forward(self):
+        def forwa(self):
             self.t.forward(1)
         def back(self):
             self.t.back(1)
@@ -21,23 +21,38 @@ class entity:
             self.t.left(1)
         
     class enemy:
-        def __init__(self):
-            self.t = [turtle.Turtle(),turtle.Turtle(),turtle.Turtle]
-        def enemy_1():
-            pass
+        def __init__(self,mengde):
+            self.t = []
+            self.mengde = mengde
+            for i in range(mengde):
+                self.t.append(turtle.Turtle())
+
+        
+        def enemy(self):
+            for i in range(self.mengde):
+                dire = random.randint(1,4)
+                num = random.randint(1,50)
+                if dire == 1:
+                    for j in range(num):
+                        self.t[i].forward(1)
+                elif dire == 2:
+                    for j in range(num):
+                        self.t[i].back(1)
+                elif dire == 3:
+                    self.t[i].left(num)
+                
+                elif dire == 4:
+                    self.t[i].right(num)
+                
 
 def mainloop():
     scr = turtle.Screen()
     player = entity.player()
     while True:
-        scr.onkeypress(player.forward, 'w')
-        scr.onkeypress(player.back, 's')
-        scr.onkeypress(player.left, 'a')
-        scr.onkeypress(player.right, 'd')
-        scr.onkeypress(player.forward, 'Up')
-        scr.onkeypress(player.back, 'Down')
-        scr.onkeypress(player.left, 'Left')
-        scr.onkeypress(player.right, 'Right')
+        scr.onkeypress(player.forwa,'w');scr.onkeypress(player.forwa, 'Up')
+        scr.onkeypress(player.back, 's');scr.onkeypress(player.back, 'Down')
+        scr.onkeypress(player.left, 'a');scr.onkeypress(player.left, 'Left')
+        scr.onkeypress(player.right,'d');scr.onkeypress(player.right, 'Right')
         scr.listen()
         scr.update()
 
